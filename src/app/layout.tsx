d877531e6315d65531576bc5e3b215f1449d1128
660610778Pithiwit <141812674+Pithiwit@users.noreply.github.com>
2024-09-04 08:52:58 +0000

    finishall

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            year ="2024"
+            fullName="Pithiwit Netkhamyuang"
+            studentId="660610778"
           />
         </div>
       </body>
